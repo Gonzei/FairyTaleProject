580f3102ff83d1ce9caad3b3cfd6557a71b458de
Gonzei <hawkeyejal@gmail.com>
2023-12-13 01:57:05 -0500

    bug fixes

--- Mynotaur/Assets/Scripts/GameManager.cs
+++ Mynotaur/Assets/Scripts/GameManager.cs
@@ -6,9 +6,24 @@ using UnityEngine.SceneManagement;
 public class GameManger : MonoBehaviour
 {
     public int currentSceneIndex;
+
+    [SerializeField] public GameObject pauseMenu;
     private void Start()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+
+        if(Input.GetKeyUp(KeyCode.Escape)) 
+        {
+            pauseMenu.SetActive(false);
+        }
     }
 
     public void PlayGame()
